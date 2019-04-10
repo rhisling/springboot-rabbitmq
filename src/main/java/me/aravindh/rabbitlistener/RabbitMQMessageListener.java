caae2a9753b14ff286f119aef7c0ab0219fe4e1f
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-10 11:39:50 -0700

    Fixing listener

--- src/main/java/me/aravindh/rabbitlistener/RabbitMQMessageListener.java
+++ src/main/java/me/aravindh/rabbitlistener/RabbitMQMessageListener.java
@@ -3,11 +3,10 @@ package me.aravindh.rabbitlistener;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 
-import java.util.Arrays;
-
 public class RabbitMQMessageListener implements MessageListener {
     @Override
     public void onMessage(Message message) {
-        System.out.println("message= [" + Arrays.toString(message.getBody()) + "]");
+        byte[] body = message.getBody();
+        System.out.println("message= [" + new String(body) + "]");
     }
 }
