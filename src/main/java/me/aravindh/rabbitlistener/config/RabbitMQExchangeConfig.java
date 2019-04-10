fdd166221fa0ca9da885a38289423c76e944a6c4
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-10 11:03:12 -0700

    Adding exchange config

---
+++ src/main/java/me/aravindh/rabbitlistener/config/RabbitMQExchangeConfig.java
@@ -0,0 +1,25 @@
+package me.aravindh.rabbitlistener.config;
+
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQExchangeConfig {
+
+    @Bean
+    Exchange exampleExchange() {
+        return new TopicExchange("ExampleExchange");
+    }
+
+    @Bean
+    Exchange example2ndExchange() {
+        return ExchangeBuilder
+                .directExchange("Example2ndExchange")
+                .autoDelete()
+                .internal()
+                .build();
+    }
+}
